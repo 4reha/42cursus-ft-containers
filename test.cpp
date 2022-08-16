bef4c99bae547f43f7e3f2c65ca71269824b877b
4reha <ael-hadd@student.1337.ma>
2022-08-16 16:21:26 +0100

    adding tests

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hadd <ael-hadd@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 10:25:07 by ael-hadd          #+#    #+#             */
+/*   Updated: 2022/08/16 11:54:48 by ael-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iterator>
+#include <vector>
+#include <iostream>
+#include <list>
+
+int main(void) 
+{
+typedef std::iterator_traits<std::vector<int>:: iterator> traits;
+	if (typeid(traits::iterator_category)==typeid(std::random_access_iterator_tag))
+		std::cout << "int* is a random-access iterator";
+	return 0;
+}
