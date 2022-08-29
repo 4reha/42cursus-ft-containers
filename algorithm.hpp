4ced2ae23bc754bd1f43fdefecce582268822f9b
4reha <ael-hadd@student.1337.ma>
2022-08-29 13:40:03 +0100

    Algo utils

--- algorithm.hpp
+++ algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-hadd <ael-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:15:00 by ael-hadd          #+#    #+#             */
-/*   Updated: 2022/08/27 10:17:20 by ael-hadd         ###   ########.fr       */
+/*   Updated: 2022/08/29 13:30:07 by ael-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 namespace ft
 {
+
 	template <typename T>
 	void	swap(T& x, T& y)
 	{
@@ -23,5 +24,107 @@ namespace ft
 		x = y;
 		y = tmp;
 	}
-	
+
+	template <class InputIterator1, class InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	{
+		while (first1 != last1)
+		{
+			if (*first1 != *first2)
+				return (false);
+			first1++;
+			first2++;
+		}
+		return (true);
+	};
+
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	{
+		while (first1 != last1)
+		{
+			if (!pred(*first1, *first2))
+				return (false);
+			first1++;
+			first2++;
+		}
+		return (true);
+	};
+
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return (false);
+			else if (*first < *first2)
+				return (true);
+			first1++;
+			first2++;
+		}
+		return (first2 != last2);
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1))
+				return (false);
+			else if (comp(*first, *first2))
+				return (true);
+			first1++;
+			first2++;
+		}
+		return (false);
+	}
+
+	template< class T1, class T2>
+	struct pair
+	{
+		typedef T1 	first_type;
+		typedef T2 	second_type;
+
+		T1 first;
+		T2 second;
+
+		pair() : first(), second() {}
+		pair(const T1& x, const T2& y) : first(x), second(y) {};
+		template<class U1, class U2>
+		pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {};
+		pair& operator=(const pair& other) {
+			first = other.first;
+			second = other.second;
+			return *this;
+		}	
+	};
+	template< class T1, class T2 >
+	ft::pair<T1,T2> make_pair( T1 t, T2 u )
+	{ return (pair<T1,T2>(t,u)); }
+
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return (lhs.first == rhs.first && lhs.second == rhs.second); }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs == rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second)); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs < lhs); }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return (rhs < lhs); }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs < rhs); }
 }
