000235c576a3034eeec5eacdb914f165213b6de0
4reha <ael-hadd@student.1337.ma>
2022-09-02 19:45:17 +0100

    red black delete fixing

--- red_black.hpp
+++ red_black.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-hadd <ael-hadd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 09:31:53 by ael-hadd          #+#    #+#             */
-/*   Updated: 2022/08/31 14:25:30 by ael-hadd         ###   ########.fr       */
+/*   Updated: 2022/09/02 19:19:58 by ael-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 #include	<iostream>
 #include "iterators.hpp"
+#include "functional.hpp"
 
 namespace	ft
 {
 
-	enum node_color {BLACK, RED};
+	enum node_color {BLACK, RED, DOUBLE_BLACK};
 
 	
 	template<typename T>
@@ -28,11 +29,14 @@ namespace	ft
 		typedef Node<T>*	node_pointer;
 
 		value_type		key;
+		node_color		color;
 		node_pointer		parent;
 		node_pointer		right;
 		node_pointer		left;
-		node_color		color;
 		
+
+		Node(value_type& data, node_color color = RED) 
+			: key(data), color(color), parent(nullptr), right(nullptr), left(nullptr) {}
 		bool	isRoot()	{ return (!parent); }
 		bool isLeft()	{ return (parent && parent->left == this); }
 		bool	isRight()	{ return (parent && parent->right == this); }
@@ -41,60 +45,94 @@ namespace	ft
 		
 	};
 
-	template<typename T>
+	template<class T, class Compare = ft::less<T>, class Alloc = std::allocator<ft::Node<T> > >
 	class RBTree
 	{
 		public:
-			typedef typename	ft::Node<T>::value_type					value_type;
-			typedef typename	ft::Node<value_type>					node_type;
-			typedef typename	ft::Node<value_type>::node_pointer			node_pointer;
-			typedef typename	ft::RBIterator<node_pointer,value_type>		iterator;
+			typedef T											value_type;
+			typedef Alloc										allocator_type;
+			typedef ft::Node<value_type>							node_type;
+			typedef typename ft::Node<value_type>::node_pointer		node_pointer;
+			typedef ft::RBIterator<node_pointer,value_type>			iterator;
+			typedef ft::RBIterator<node_pointer,const value_type>		const_iterator;
+			typedef ft::reverse_iterator<iterator>					reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>				const_reverse_iterator;
+			typedef size_t										size_type;
 		public:
-			node_pointer	root;
-			size_t		_size;
+			node_pointer		root;
+			node_pointer		DB;
+			allocator_type		_allocator;
+			Compare			_comp;
+			size_type			_size;
+		protected:
+				void	leftRotate(node_pointer ptr)	{
+				// std::cout << "leftRotate" << std::endl;
+				node_pointer	r_node = ptr->right;
+				ptr->right = r_node->left;
+				if (r_node->left)
+					r_node->left->parent = ptr;
+				r_node->parent = ptr->parent;
+				if (!ptr->parent)
+					this->root = r_node;
+				else	if (ptr->isLeft())
+					ptr->parent->left = r_node;
+				else 
+					ptr->parent->right = r_node;
+				r_node->left = ptr;
+				ptr->parent = r_node;
+			}
+
+			void	rightRotate(node_pointer ptr)	{
+				// std::cout<< "rightRotate" << std::endl;
+				node_pointer	l_node = ptr->left;
+				ptr->left = l_node->right;
+				if (l_node->right)
+					l_node->right->parent = ptr;
+				l_node->parent = ptr->parent;
+				if (!ptr->parent)
+					this->root = l_node;
+				else if (ptr->isLeft())
+					ptr->parent->left = l_node;
+				else
+					ptr->parent->right = l_node;
+				l_node->right = ptr;
+				ptr->parent = l_node;
+			}
+			void	reColor()
+				{ this->color = BLACK ? this->color == RED : this->color = RED; }
+			node_pointer	minNode(node_pointer root)
+			{
+				node_pointer node = root;
+				while (node->left)
+					node = node->left;
+				return (node);
+			}
+			
+			node_pointer	maxNode(node_pointer root)
+			{
+				node_pointer node = root;
+				while (node->right)
+					node = node->right;
+				return (node);
+			}
+			
 		public:
 			RBTree() { this->root = nullptr; _size = 0;}
 			void	insert(value_type key)
 			{
-				node_pointer newNode = new node_type;
-				newNode->key = key;
-				newNode->parent = nullptr;
-				newNode->right = nullptr;
-				newNode->left = nullptr;
-				newNode->color = RED;
-				node_pointer p = nullptr;
-				node_pointer x = this->root;
-				this->_size++;
-				while (x)
-				{
-					p = x;
-					if (newNode->key < x->key)
-						x = x->left;
-					else
-						x = x->right;
-				}
-				newNode->parent = p;
-				if (!p)
-				{
-					this->root = newNode;
-					this->root->color = BLACK;
-					return ;
-				}
-				else if (newNode->key < p->key)
-					p->left = newNode;
-				else
-					p->right = newNode;
+				_size++;
+				node_pointer newNode = _allocator.allocate(1);
+				_allocator.construct(newNode, ft::Node<value_type>(key));
+				this->root = BSTinsertion(this->root, newNode);
+				this->root->color = BLACK;
 				insertionFix(newNode);
 			}
 			void	insertionFix(node_pointer node)	{
 				node_pointer uncle;
-				while (node->parent->color == RED)	{
-					std::cout << node->parent->key << std::endl;
-					std::cout << node->key << std::endl;
-					
+				while (!node->isRoot() && node->parent->isRed())	{					
 					if (node->parent->isRight())	{
 						uncle = node->parent->parent->left;
-						if (uncle && uncle->color == RED)	{
+						if (uncle && uncle->isRed())	{
 							// case 3.1
 							uncle->color = BLACK;
 							node->parent->color = BLACK;
@@ -116,7 +154,7 @@ namespace	ft
 					else	{
 					// std::cout << "Here" << std::endl;
 						uncle = node->parent->parent->right;
-						if (uncle && uncle->color == RED)	{
+						if (uncle && uncle->isRed())	{
 							// mirror case 3.1
 							uncle->color = BLACK;
 							node->parent->color = BLACK;
@@ -136,89 +174,106 @@ namespace	ft
 						}
 
 					}
-					if (node == this->root)	{
-						std::cout << "root" << std::endl;
-						node->color = BLACK;
-						break ;
-					}
 				}
+				this->root->color = BLACK;
 			}
 			
-			void	remove(value_type key)
+			node_pointer BSTinsertion(node_pointer ptr, node_pointer node)
 			{
-				node_pointer node = this->root;
-				node_pointer tmp = nullptr;
-				node_pointer p, q;
+				if (!ptr)	{
+					ptr = node;
+				}
+				else if (_comp(node->key, ptr->key))	{
+					ptr->left = BSTinsertion(ptr->left, node);
+					ptr->left->parent = ptr;
+				}
+				else {
+					ptr->right = BSTinsertion(ptr->right, node);
+					ptr->right->parent = ptr;
+				}
+				return (ptr);
+			}
+			
+			node_pointer searchNode(node_pointer root, value_type key)
+			{
+				node_pointer node = root;
 				while (node)
 				{
-					if (node->key == key)	{
-						tmp = node;
+					if (node->key == key)
 						break ;
-					}
-					if (node->key < key)
-						node = node->right;
-					if (node->key > key)
+					if (_comp(key, node->key))
 						node = node->left;
+					else
+						node = node->right;
 				}
-				if (!tmp)
-					return;
-				q = tmp;
+				return (node);
+			}
+			void rbTransplant(node_pointer n1, node_pointer n2)
+			{
+				n1->key = n2->key;
+				if (n2->left)
+				{
+					n2->parent = n2->left;	
+					n2->left->parent = n2->parent;
+				}
+			}
+			void	remove(value_type key)
+			{
+				node_pointer node = searchNode(this->root, key);
+				if (node)
+				{
+					if (node->right)
+					{
+						if (node->isRoot())
+						{
+							this->root = node->right;
+							node->right->parent = nullptr;	
+						}
+						else if (node->isLeft())
+						{
+							node->parent->left = node->right;
+							node->right->parent = node->parent;	
+						}
+						else if (node->isRight())
+						{
+							node->parent->right = node->right;
+							node->right->parent = node->parent;	
+						}
+					}
+					if (node->left)
+					{
+						node_pointer to_replace = maxNode(node->left);
+						rbTransplant(node, to_replace);
+						int c = to_replace->color;
+						delete to_replace;
+					}
+					else
+					{
+						int c = node->color;
+						delete node;
+					}
+				}
 				
 				
 			}
 
-			iterator	begin()	
-			{
-				node_pointer t = this->root;
-				while (t->left)
-					t = t->left;
-				return (iterator(t));
-			}
-			iterator	end()	
-			{
-				node_pointer t = this->root;
-				while (t->right)
-					t = t->right;
-				return (iterator(t));
-			}
 
-
-			void	leftRotate(node_pointer ptr)	{
-				std::cout << "leftRotate" << std::endl;
-				node_pointer	r_node = ptr->right;
-				ptr->right = r_node->left;
-				if (r_node->left)
-					r_node->left->parent = ptr;
-				r_node->parent = ptr->parent;
-				if (!ptr->parent)
-					this->root = r_node;
-				else	if (ptr->isLeft())
-					ptr->parent->left = r_node;
-				else 
-					ptr->parent->right = r_node;
-				r_node->left = ptr;
-				ptr->parent = r_node;
-			}
-
-			void	rightRotate(node_pointer ptr)	{
-				std::cout<< "rightRotate" << std::endl;
-				node_pointer	l_node = ptr->left;
-				ptr->left = l_node->right;
-				if (l_node->right)
-					l_node->right->parent = ptr;
-				l_node->parent = ptr->parent;
-				if (!ptr->parent)
-					this->root = l_node;
-				else if (ptr->isLeft())
-					ptr->parent->left = l_node;
-				else
-					ptr->parent->right = l_node;
-				l_node->right = ptr;
-				ptr->parent = l_node;
-			}
-			void	reColor()
-				{ this->color = BLACK ? this->color == RED : this->color = RED; }
-			
-		
+			// Iretators:
+			iterator begin()	{
+				return (iterator(minNode(this->root))); }
+			const_iterator begin() const	{
+				return (iterator(minNode(this->root))); }
+			iterator end()	{
+				return (iterator(maxNode(this->root))); }
+			const_iterator end() const	{
+				return (iterator(maxNode(this->root))); }
+			reverse_iterator rbegin()	{
+				return (reverse_iterator(maxNode(this->root))); }
+			const_reverse_iterator rbegin() const	{
+				return (reverse_iterator(maxNode(this->root))); }
+			reverse_iterator rend()	{
+				return (reverse_iterator(minNode(this->root))); }
+			const_reverse_iterator rend() const	{
+				return (reverse_iterator(minNode(this->root))); }
 	};
 }
